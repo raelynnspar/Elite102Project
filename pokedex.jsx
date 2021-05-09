6c07c3a6fabee880bb9d854ef7b03d4dcbdfa9cd
KRMS1638 <sparr.katier@gmail.com>
2021-05-09 00:18:59 -0500

    Initial fetch attempt

---
+++ pokedex.jsx
@@ -0,0 +1,39 @@
+
+function App() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    
+    useEffect(() => {
+      fetch("https://pokeapi.co/api/v2/pokedex/")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+     
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} 
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import './App.css';
+
+
+
+const App = () => {
+  
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+     
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return(
+    <div>
+
+    </div>
+  );
+
+
+};
+
+
+  
+
+export default App;
+
