34dd3dffc28d1bda8eb30a1cd27e5695b8816cfc
KRMS1638 <sparr.katier@gmail.com>
2021-05-16 23:25:25 -0500

    Set route to the detailed page of the pokemon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 
 import NavBar from "./components/layout/NavBar";
 import Dashboard from "./components/layout/Dashboard";
+import Pokemon from '.?components/pokemon/Pokemon';
 
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className = "App">
           <NavBar />
         <div className = "container">
+        <Switch>
+          <Route exact path = "/" component = {Dashboard} />
+          <Route exact path = "/pokemon/:pokemonIndex" component={Pokemon} />
           <Dashboard />
+          </Switch>  
         </div>
       </div>
+      </Router>
     )
   }
 }
